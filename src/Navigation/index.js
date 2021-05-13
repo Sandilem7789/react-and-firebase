90e553c1f34d749fa57e29b5eac89b804efa0fc9
sandilem7789 <sandilem7789@gmail.com>
2021-05-13 11:42:10 +0200

    Navigation

---
+++ src/Navigation/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import * as ROUTES from "../constants/routes";
+
+const Navigation = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link to={ROUTES.LANDING}>Landing</Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link to={ROUTES.HOME}>Home</Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link to={ROUTES.ACCOUNT}>Account</Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link to={ROUTES.ADMIN}>Admin</Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation
