4e95d214b6d56ac6c27fc16a31e05daa2c11f34f
sandilem7789 <sandilem7789@gmail.com>
2021-05-20 09:41:01 +0200

    env file bug

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,10 +1,34 @@
-import "./process.env";
-const firebaseConfig = {
-    apiKey: "AIzaSyC7BQgyHZedgf66yCvB9E1Tljh88PmVQzA",
-    authDomain: "react-road-data.firebaseapp.com",
-    databaseURL: "https://react-road-data-default-rtdb.firebaseio.com/",
-    projectId: "react-road-data",
-    storageBucket: "react-road-data.appspot.com",
-    messagingSenderId: "838224745547",
-    appId: "1:838224745547:web:8ddef29df9ff67ebb5e9ce"
-  };
+import app from "firebase/app";
+import "firebase/auth";
+
+
+console.log(process.env.REACT_APP_DATABASE_URL);
+
+const config = {
+  apiKey: "AIzaSyC7BQgyHZedgf66yCvB9E1Tljh88PmVQzA",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+class Firebase {
+  constructor(){
+    app.initializeApp(config);
+    
+    this.auth = app.auth();
+  }
+
+  // ** Authentication API **
+  doCreateUserWithEmailPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+  
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
